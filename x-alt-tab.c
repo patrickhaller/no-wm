d8237405dd8cecd3f8f313131e8c214b123b2948
Patrick Haller <patrick_haller@ofs.edu.sg>
2011-11-09 11:05:01 +0800

    style cleanup

--- x-alt-tab.c
+++ x-alt-tab.c
@@ -4,13 +4,15 @@ int main(int argc, char **argv)
 {
 	Display *dpy;
 
-	if(!(dpy = XOpenDisplay(0x0))) return 1;
+	if ( (dpy = XOpenDisplay(NULL)) == NULL)
+		return 1;
+
 	if (argc > 1)
 		XCirculateSubwindowsDown(dpy, DefaultRootWindow(dpy));
 	else
 		XCirculateSubwindowsUp(dpy, DefaultRootWindow(dpy));
-	XSetInputFocus(dpy, PointerRoot, RevertToPointerRoot, CurrentTime); /* nuke focus back to pointer */
+
+	XSetInputFocus(dpy, PointerRoot, RevertToPointerRoot, CurrentTime);
 	XSync(dpy, True);
 	return 0;
 }
-
