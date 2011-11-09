d4e2361215ba3288d4ed71dabdc9a7399654db49
Patrick Haller <patrick_haller@ofs.edu.sg>
2011-11-09 11:34:34 +0800

    style cleanup - switched to pointer math

--- x-window-list.c
+++ x-window-list.c
@@ -6,24 +6,23 @@
 // first window listed is top-most, last is bottom-most in window-stacking
 int main(void) {
 	Display *dpy;
-	Window root;
-	Window parent;
-	Window *wins;
+	Window root, parent, *wins, *w;
 	unsigned int nwins;
 
-	if(!(dpy = XOpenDisplay(NULL)))
+	if ((dpy = XOpenDisplay(NULL)) == NULL)
 		return 1;
+
 	XQueryTree(dpy, DefaultRootWindow(dpy), &root, &parent, &wins, &nwins);
 
-	for (int i=nwins-1; i >= 0; i--) {
+	for (w = wins + nwins - 1; w >= wins; w--) {
 		XTextProperty name;
 		XClassHint hint;
 		XWindowAttributes attr;
 
-		XGetClassHint(dpy, wins[i], &hint);
-		XGetWMName(dpy, wins[i], &name);
-		XGetWindowAttributes(dpy, wins[i], &attr);
+		XGetClassHint(dpy, *w, &hint);
+		XGetWMName(dpy, *w, &name);
+		XGetWindowAttributes(dpy, *w, &attr);
 		if (attr.map_state == IsViewable)
-			fprintf(stdout, "0x%-10x %s - %s\n", (unsigned int)wins[i], hint.res_name, name.value);
+			printf("0x%-10x %s - %s\n", (unsigned int)*w, hint.res_name, name.value);
 	}
 }
