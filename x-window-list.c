ed7f297208fe1d1e1dedac1bba275be1beabe869
Patrick <devnull@mailinator.com>
2015-10-15 14:54:50 +0800

    split into args parsing + sanity and actions

--- x-window-list.c
+++ x-window-list.c
@@ -8,58 +8,73 @@ worldwide. This software is distributed without any warranty. You should have
 received a copy of the CC0 Public Domain Dedication along with this software.
 If not, see http://creativecommons.org/publicdomain/zero/1.0/ */
 
+// http://tronche.com/gui/x/xlib/
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// http://tronche.com/gui/x/xlib/
-// first window listed is top-most, last is bottom-most in window-stacking
+typedef struct Config {
+	_Bool is_switch; // list windows or switch to dst window?
+	unsigned int dst;
+} Config;
 
-static int cmp(Window **a, Window **b) {
+/* static int cmp(Window **a, Window **b) {
 	if ( **a > **b )
 		return 1;
 	return -1;
+} */
+
+void x_window_list(Config cfg, Display *dpy, Window *wins, unsigned int nwins) {
+	Window *viewables[nwins], *w;
+	unsigned int vc = 0;
+
+	// get list of viewable windows
+	for (w = wins + nwins - 1; w >= wins; w--) {
+		XWindowAttributes attr;
+
+		XGetWindowAttributes(dpy, *w, &attr);
+		if (attr.map_state == IsViewable) {
+			viewables[vc++] = w;
+		}
+	}
+	viewables[vc] = NULL;
+
+	// list them out
+	for (int i=0; i < vc; i++) {
+		XTextProperty name;
+		XClassHint hint;
+		w = viewables[i];
+
+		XGetClassHint(dpy, *w, &hint);
+		XGetWMName(dpy, *w, &name);
+		printf("%02d 0x%-12x %s - %s\n", i, (unsigned int)*w, hint.res_name, name.value);
+		if (cfg.is_switch && i == cfg.dst) {
+			XRaiseWindow(dpy, *w);
+			XSetInputFocus(dpy, *w, RevertToPointerRoot, CurrentTime);
+		}
+	}
 }
 
 int main(int argc, char **argv) {
 	Display *dpy;
-	Window root, parent, *wins, *w, *winlist[256];
-	unsigned int nwins, count = 0, dwin = 0;
+	Window root, parent, *wins;
+	unsigned int nwins = 0;
+	Config c = { .is_switch = 0, .dst = 0 };
 
-	if (argc > 1)
-		dwin = atoi(argv[1]);
+	if (argc > 1) {
+		c.is_switch = 1;
+		c.dst = atoi(argv[1]);
+	}
 
 	if ((dpy = XOpenDisplay(NULL)) == NULL)
 		return 1;
 
 	XQueryTree(dpy, DefaultRootWindow(dpy), &root, &parent, &wins, &nwins);
+	if (nwins == 0)
+		return 0;
 
-	for (w = wins + nwins - 1; w >= wins; w--) {
-		XWindowAttributes attr;
-
-		XGetWindowAttributes(dpy, *w, &attr);
-		if (attr.map_state != IsViewable)
-			continue;
-
-		winlist[count] = w;
-		count++;
-	}
-	qsort(winlist, count, sizeof(Window *), (__compar_fn_t)cmp);
-
-	for (int i=0; i<count; i++) {
-		XTextProperty name;
-		XClassHint hint;
-		w = winlist[i];
-
-		XGetClassHint(dpy, *w, &hint);
-		XGetWMName(dpy, *w, &name);
-		printf("%02d 0x%-12x %s - %s\n", i, (unsigned int)*w, hint.res_name, name.value);
-		if (argc > 1 && i == dwin) {
-			XRaiseWindow(dpy, *w);
-			XSetInputFocus(dpy, PointerRoot, RevertToPointerRoot, CurrentTime);
-			XSync(dpy, DefaultRootWindow(dpy));
-		}
-	}
+	x_window_list(c, dpy, wins, nwins);
+	return 0;
 }
